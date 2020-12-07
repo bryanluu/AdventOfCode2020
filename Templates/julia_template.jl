5a27b7dcf89d40d1ce69360c4d80de07e8b436ca
Bryan Luu <loc.bryan.luu@gmail.com>
2020-12-07 11:08:58 -0800

    Added template files for Julia, Python, and Ruby.

---
+++ Templates/julia_template.jl
@@ -0,0 +1,12 @@
+filename = "input"; # Input file
+
+# Solves the challenge with input filename
+function solve(filename)
+  
+end
+
+# run if script is run
+if abspath(PROGRAM_FILE) == @__FILE__
+  println("Input file: $(filename)")
+  @time solve(filename); # Time the solve attempt
+end
---
+++ Templates/python_template.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+filename = "input"
+
+def solve(filename):
+  pass # DO STUFF
+
+if __name__ == '__main__':
+  print(f"Input file: {filename}")
+  import time
+  start = time.time()
+  solve(filename)
+  end = time.time()
+  print(f"Solve time: {end-start} seconds")
