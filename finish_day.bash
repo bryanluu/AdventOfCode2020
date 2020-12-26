6927c2768fec85d9a94853178319b721effe2e22
Bryan Luu <loc.bryan.luu@gmail.com>
2020-12-26 15:21:33 -0800

    Updated the start/finish scripts to include a choice of day, and it does 1 part at a time unless '-a' is specified to the finish script. Added a 'Part(s)' column to 'times.csv'.

--- finish_day.bash
+++ finish_day.bash
@@ -5,7 +5,7 @@ end=$(date)
 folder=${PWD##*/}
 if [ ${folder//[0-9]/} != Day ]
 then
-echo "Cannot run this yet! Start by running '. start_next.bash' first..."
+echo "Cannot run this yet! Start by running '. start_next.bash [day]' first..."
 exit 1
 else
 
@@ -15,14 +15,27 @@ timefile="../times.csv"
 if [ ! -f $timefile ]
 then
 
-echo "Day,Start,End" > $timefile
-echo >> $timefile
+echo "Day,Part(s),Start,End" > $timefile
 
 fi
 
+# get number of parts already saved for this day
+entries=$(egrep -c -e "^$day,1,.*$" $timefile)
+
+# check if all option is provided
+parts="$(($entries+1))"
+partstr=""
+for arg in $@
+do
+  if [ $arg == "-a" ] || [ $arg == "--all" ]
+  then
+    parts="1+2"
+    partstr="s"
+  fi
+done
+
 st=$(head start)
-echo "$day,$st,$end" >> $timefile
-echo >> $timefile
+echo "$day,$parts,$st,$end" >> $timefile
 
 rm start
 
@@ -34,17 +47,18 @@ esac
 
 dir=$PWD
 
-echo "Congrats on completing the Day $day! :)"
+echo "Congrats on completing the Day $day, part$partstr $parts! :)"
 echo "Times saved in '$(dirname $PWD)/times.csv'"
 
 mv $0 ..
 
 git add $dir $timefile
-git commit -m "Added solution for December $day and updated times.csv"
+git commit -m "Added solution for Day $day, Part$partstr $parts and updated times.csv"
 git commit --amend
 
 # if aborted, undo commit but keep files
 if [ $? == 1 ]; then
+echo "Did not commit..."
 git reset HEAD~
 fi
 
--- start_next.bash
+++ start_next.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 # This file should be called using `source` rather than bash
 
+advent_year=2020
 finish="finish_day.bash"
 if [ -f $finish ]
 then
 
+year=$(date +%Y)
 month=`date +%m`
-if [ $month != 12 ] # Invalid month
+if ([ $year == $advent_year ] && [ $month != 12 ])\
+   || [ $(($year < $advent_year)) == 1 ] # Too early
 then
-echo "Invalid month!"
+echo "Advent of Code $advent_year hasn't started yet!"
 else
 
 today=`date +%d`
@@ -19,37 +22,68 @@ today=${today#0}
 hour=$(date +%H)
 if [ $(($days > $today)) == 1 ] || ([ $today == $days ] && [ $(($hour < 21)) == 1 ])
 then
-echo "Too early! Wait until December $(($days)), 9pm PST for the next challenge."
-elif [ $today -ge 25 ]
-then
-echo "Advent already finished! (Good job)"
+echo "Too early! Wait until December $(($days)) $advent_year"\
+", 9pm PST for the next challenge."
 else
 
-
 day=$(($days+1))
-echo "Starting Day $day..."
-newdir="Day$day"
+
+timefile="times.csv"
+
+skip=false
+# # set custom day if given and it is not yet finished
+if [[ $1 =~ [0-9]+ ]] && [ $((8<=$1 && $1<=25)) == 1 ];
+then
+  if [ -f $timefile ]; then
+    # Check if part 2 for the given day is not yet finished
+    if [ $(egrep -c -e "^$1,(1\+)?2,.*$" $timefile) == 0 ]; then
+      day=$1
+    else
+      echo "Day $1 already completed..."
+      read -p $"Continue onto Day $day?"$'\n' reply
+      [[ $reply =~ ^[Yy].*$ ]] || skip=true # skip if reply is No
+      [ $skip == true ] && echo "Stopping..."
+    fi
+  else
+    day=$1
+  fi
+elif [ $(($day > 25)) == 1 ] && [ -z $1 ]; then
+  echo "Day required now..."
+  echo "Usage: . start_next.bash day"
+  skip=true
+else
+  echo "Invalid day given: $1"
+fi
+
+if [ $skip == false ]; then
+
+echo "Working on Day $day..."
+dir="Day$day"
 case $(($day%3)) in
   0) prog="ruby"; ext="rb";;
   1) prog="julia"; ext="jl";;
   2) prog="python"; ext="py";;
 esac
-newfile="Day$day.$ext"
+codefile="Day$day.$ext"
 
-mkdir $newdir
-mv $finish $newdir
-cp "Templates/${prog}_template.$ext" "$newdir/$newfile"
-cd $newdir
+mkdir -p $dir
+mv $finish $dir
+
+[ ! -f "$dir/$codefile" ] && cp "Templates/${prog}_template.$ext"\
+   "$dir/$codefile" # Copy template if the file doesn't exist
+cd $dir
 
 date > start
 
-echo "Run './finish_day.bash' to complete solution."
+echo "Run './finish_day.bash [-a OR --all]' to complete solution."
 
 echo '#!/bin/bash' > run.bash
-echo "$prog $newfile" '$1' >> run.bash
-chmod +x run
+echo "$prog $codefile" '$1' >> run.bash
+chmod +x run.bash
 
-code $newfile # start editing
+code $codefile # start editing
+
+fi
 
 fi
 
@@ -60,7 +94,7 @@ else
 unfinished=`dirname */$finish`
 day=${unfinished//[!0-9]/}
 echo "Cannot start until Day $day is completed!"
-echo "Run ./run.bash to test program..."
-echo "Run './finish_day.bash' in '$unfinished/' to finish first..."
+echo "Run './run.bash [filename]' to test program..."
+echo "Run './finish_day.bash [-a OR --all]' in '$unfinished/' to finish first..."
 
 fi
