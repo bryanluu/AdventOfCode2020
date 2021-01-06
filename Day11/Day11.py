b552986d83d0c596384371d0ececccaa451964ab
Bryan Luu <loc.bryan.luu@gmail.com>
2021-01-05 18:56:50 -0800

    Improved Day 11: part 1 solved in 30 seconds, part 2 solved in 37!

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -3,6 +3,12 @@ import sys
 import numpy as np
 filename = sys.argv[1] if len(sys.argv) > 1 else "input"
 
+part = (int(sys.argv[2]) if len(sys.argv) > 2 else None)
+while part is None:
+  print("Part 1 or 2?")
+  reply = input("Choose: ")
+  part = (int(reply) if reply == "1" or reply == "2" else None)
+
 # characters in problem statement
 FLOOR = "."
 EMPTY = "L"
@@ -15,13 +21,17 @@ def show_grid(grid):
 
 # Gets the adjacent surrounding seats of seat at r, c
 def get_adjacent(seats, r, c):
+  neighbors = np.zeros(seats.shape, dtype=bool)
   rows, cols = seats.shape
   adjacent = np.array([(r+i, c+j) for i in range(-1, 2) for j in range(-1, 2)
     if 0 <= r+i < rows and 0 <= c+j < cols and not (i == 0 and j == 0)])
-  return adjacent
+  for nr, nc in adjacent:
+    neighbors[nr, nc] = True
+  return adjacent, neighbors
 
 # Gets visible seats from seat at r, c
 def get_visible_chairs(seats, r, c):
+  neighbors = np.zeros(seats.shape, dtype=bool)
   rows, cols = seats.shape
   valid = lambda row, col: (0 <= row < rows and 0 <= col < cols and not (row == r and col == c)
     and seats[row, col] != FLOOR)
@@ -37,42 +47,37 @@ def get_visible_chairs(seats, r, c):
   visible = np.stack((first(W), first(E), first(N), first(S),
     first(NW), first(NE), first(SE), first(SW)))
   ok = np.all(visible >= 0, axis=1)
-  return visible[ok]
+  for nr, nc in visible[ok]:
+    neighbors[nr, nc] = True
+  return visible[ok], neighbors
 
-# Get potential seats that might change
-def get_potential_changed(seats, positions, changed, part):
-  check = changed.copy()
-  for r, c in positions[changed]:
-    neighbors = (get_adjacent(seats, r, c) if part == 1 else get_visible_chairs(seats, r, c))
-    for neighbor in neighbors:
-      nr, nc = neighbor
-      check[nr, nc] = True if seats[nr, nc] != FLOOR else False
-  return check
+def initialize_neighbors(seats, positions):
+  neighbors = {}
+  for r, c in positions[seats != FLOOR]:
+    neighbors[r, c] = (get_adjacent(seats, r, c) if part == 1 else get_visible_chairs(seats, r, c))
+  return neighbors
 
 # Simulate one round of seating
-def simulate_round(seats, positions, check, part):
+def simulate_round(seats, positions, neighbors):
+  limit = (4 if part == 1 else 5)
   simulation = seats.copy()
+  flipped = np.where(seats == EMPTY, np.full(seats.shape, OCCUPIED), 
+    np.where(seats == OCCUPIED, np.full(seats.shape, EMPTY), np.full(seats.shape, FLOOR)))
   changed = np.zeros(seats.shape, dtype=bool)
+  occupied = np.zeros(seats.shape, dtype=int)
   if np.all(seats != OCCUPIED): # if no seats are occupied
     chairs = (seats == EMPTY)
     simulation[chairs] = OCCUPIED
     changed[chairs] = True
   else: # else check each position that might change
-    limit = (4 if part == 1 else 5)
-    for r, c in positions[check]:
-      seat = seats[r, c]
-      neighbors = (get_adjacent(seats, r, c) if part == 1 else get_visible_chairs(seats, r, c))
-      neighbors_seats = np.array([seats[i, j] for i, j in neighbors])
-      # print(f"r: {r}, c: {c}, neighbors: {neighbors}, seats: {neighbors_seats}")
-      if seat == EMPTY and np.all(neighbors_seats != OCCUPIED):
-        simulation[r, c] = OCCUPIED # occupy seat
-        changed[r, c] = True
-      elif seat == OCCUPIED and np.sum(neighbors_seats == OCCUPIED) >= limit:
-        simulation[r, c] = EMPTY # empty seat
-        changed[r, c] = True
+    for r, c in positions[seats != FLOOR]:
+      _, isneighbor = neighbors[r, c]
+      occupied[r, c] = np.sum((seats[isneighbor] == OCCUPIED))
+    changed = (((seats == EMPTY) & (occupied == 0)) | ((seats == OCCUPIED) & (occupied >= limit)))
+    simulation = np.where(changed, flipped, seats)
   return changed, simulation
 
-def solve(filename, part):
+def solve(filename):
   seats = []
   with open(filename) as file:
     for line in file:
@@ -81,19 +86,19 @@ def solve(filename, part):
   seats = np.array(seats)
   rows, cols = seats.shape
   positions = np.array([[(r, c) for c in range(cols)] for r in range(rows)])
-  check = (seats != FLOOR)
   round = 0
   print("---Initial Layout---")
   show_grid(seats)
+  print("Building neighbor list...")
+  neighbors = initialize_neighbors(seats, positions)
   print("===Simulation Begin===")
   while True:
-    changed, simulation = simulate_round(seats, positions, check, part) # simulate round
+    changed, simulation = simulate_round(seats, positions, neighbors) # simulate round
     if np.any(changed): # if any seat changed, continue, otherwise, finish
       round += 1
       print(f"---Round {round}---")
       seats = simulation
       show_grid(seats)
-      check = get_potential_changed(seats, positions, changed, part) # seats to check for status
     else:
       break
   print(f"===Simulation Ended after {round} rounds===")
@@ -103,11 +108,6 @@ if __name__ == '__main__':
   print(f"Input file: {filename}")
   import time
   start = time.time()
-  part = (int(sys.argv[2]) if len(sys.argv) > 2 else None)
-  while part is None:
-    print("Part 1 or 2?")
-    reply = input("Choose: ")
-    part = (int(reply) if reply == "1" or reply == "2" else None)
-  solve(filename, part)
+  solve(filename)
   end = time.time()
   print(f"Solve time: {end-start} seconds")
